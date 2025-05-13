d27d539e9d74d917f2e9bf30cd5489faf702ad6c
Ryujihye <131ryuji@gmail.com>
2025-05-13 19:54:51 +0900

    독하게 시작하는 C 프로그래밍 - hello world 해석(9강)

--- C_study_2025/hello.c
+++ C_study_2025/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "hello.h"
 
-int main() {
-	printf("hello world!\n");
-	return 0;
+int hello_world() { //함수, int는 함수의 타입
+	printf("hello, World!\n"); //문자(배)열, 구문을 의미
+	return 0; //반환 값
+
+	//문자열 = 문자(배열) char[15] 
+	//문자열의 첫글자로 배열의 주소를 파악함 = 배열의 이름
 }
---
+++ C_study_2025/hello.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+int hello_world(void); //선언에 해당
+
+#endif
