da991f230d5715f68f257e072a2fcbc785825629
Ryujihye <131ryuji@gmail.com>
2025-05-14 15:10:26 +0900

    독하게 시작하는 C 프로그래밍 - 표준 입출력(10강)

--- C_study_2025/hello.c
+++ C_study_2025/hello.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 #include "hello.h"
 
 int hello_world() { //함수, int는 함수의 타입
@@ -7,4 +9,111 @@ int hello_world() { //함수, int는 함수의 타입
 
 	//문자열 = 문자(배열) char[15] 
 	//문자열의 첫글자로 배열의 주소를 파악함 = 배열의 이름
+}
+
+void string_input_output() {
+	char ch = 0; //변수 선언 + 정의
+	ch = getchar(); //한 글자만 저장
+	putchar(ch);
+	putchar('Z');
+	
+	return 0;
+}
+
+void string_input_output_non_buffered() {
+	char ch = 0;
+	printf("아무 키나 누르면 다음으로 넘어갑니다.\n");
+
+	ch = _getch();
+	
+	printf("입력한 키는 ");
+	putchar(ch);
+	printf("입니다.");
+	return 0;
+}
+
+void example_string_array() {
+	char szName[32] = { 0 };
+    printf("이름을 입력하세요: ");
+
+    if (fgets(szName, sizeof(szName), stdin)) { //szName 주소 정보
+        // 개행 문자 제거
+        szName[strcspn(szName, "\n")] = 0;
+    }
+
+    printf("당신의 이름은 ");
+    puts(szName);
+    printf("입니다.\n");
+
+    return 0;
+}
+
+void form_char(void) {
+	int x = 10;
+
+	putchar('B');
+	putchar('\n');
+	printf("%c\n", 65); //A
+
+	printf("x는 %d입니다\n\a", x);
+	
+	printf("%d\n", 1234);
+	printf("%d, %d\n", 1234, -5678);
+	printf("%+d, %+d\n", 1234, -5678);
+
+	printf("%8d\n", 1234);
+	printf("%08d\n", 1234);
+	return 0;
+}
+
+void write_szBuffer(void) {
+	char szBuffer[4] = { 0 };
+	scanf_s("%4c", szBuffer, _countof(szBuffer));
+	printf("%c%c%c%c\n", szBuffer[0], szBuffer[1], szBuffer[2], szBuffer[3]);
+	return 0;
+}
+
+void what_is_your_info() {
+	char szName[32] = { 0 };
+	int nAge = 0;
+
+	printf("나이를 입력하세요. : ");
+	scanf_s("%d%*c", &nAge); // %*c = \n을 지워줌
+
+	printf("이름을 입력하세요. : ");
+	gets_s(szName, sizeof(szName));
+
+	printf("%d세 %s\n", nAge, szName);
+
+	return 0;
+}
+
+void szBuffer_left_right() {
+	char szBuffer[32] = { 0 };
+
+	//사용자가 입력한 문자열을 배열에 저장하고 출력
+	scanf_s("%s", szBuffer, (unsigned)_countof(szBuffer));
+	printf("%s\n", szBuffer);
+
+	char szBufferLeft[32] = { 0 };
+	char szBufferRight[32] = { 0 };
+
+	scanf_s("%s%s", 
+		szBufferLeft, (unsigned)_countof(szBufferLeft),
+		szBufferRight, (unsigned)_countof(szBufferRight));
+	scanf_s("%s %s \n", szBufferLeft, szBufferRight);
+}
+
+void find_your_info() {
+	char szName[20] = { 0 };
+	int szAge = 0;
+
+	printf("나이를 입력하세요: ");
+	scanf_s("%d%*c", &szAge);
+
+	printf("이름을 입력하세요: ");
+	gets_s(szName, sizeof(szName));
+
+	printf("당신의 나이는 %d살이고 이름은 '%s'입니다.", szAge, szName);
+	return 0;
 }
